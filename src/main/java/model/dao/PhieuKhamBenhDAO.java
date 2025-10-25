d5e2343e2878c8dd811d6fe4d787cd4e1a5b182f
Nguyen Dinh Tuan Dat <legendboy.vn@gmail.com>
2025-10-25 19:46:57 +0700

    add Phieu Kham Benh

--- src/main/java/model/dao/PhieuKhamBenhDAO.java
+++ src/main/java/model/dao/PhieuKhamBenhDAO.java
@@ -6,8 +6,10 @@ package model.dao;
 
 import model.Entity.PhieuKhamBenh;
 import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.List;
 import org.hibernate.Session;
-import org.hibernate.query.Query; 
+import org.hibernate.query.Query;
 import org.hibernate.Transaction;
 import util.HibernateUtil;
 
@@ -38,9 +40,36 @@ public class PhieuKhamBenhDAO {
         }
     }
 
-    //Cập nhật thông tin trong quá trình khám (thêm chẩn đoán, kết luận...).
-    public boolean updateEncounterDetails(int phieuKhamId, PhieuKhamBenh thongTinCapNhat) {
-        return false;
+    //Cập nhật thông tin trong quá trình khám .
+    public void update(PhieuKhamBenh phieuKham) {
+        Transaction transaction = null;
+        try ( Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.update(phieuKham);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(int id) {
+        Transaction transaction = null;
+        try ( Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            PhieuKhamBenh phieuKham = session.get(PhieuKhamBenh.class, id);
+            if (phieuKham != null) {
+                session.delete(phieuKham);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 
     //Xem chi tiết một lần khám.
@@ -53,21 +82,37 @@ public class PhieuKhamBenhDAO {
         }
     }
 
-    // Thêm hoặc cập nhật các chỉ số sinh tồn.
-    public boolean addVitals(int phieuKhamId, BigDecimal nhietDo, String huyetAp, Integer nhipTim, Integer nhipTho) {
-        return true;
-    }
-
     //
     public boolean isMaPhieuKhamExisted(String maPhieuKham) {
         try ( Session session = HibernateUtil.getSessionFactory().openSession()) {
             String hql = "SELECT count(p.id) FROM PhieuKhamBenh p WHERE p.maPhieuKham = :ma";
-            Query<Long> query = session.createQuery(hql,Long.class);
+            Query<Long> query = session.createQuery(hql, Long.class);
             query.setParameter("ma", maPhieuKham);
             return query.uniqueResult() > 0;
         } catch (Exception e) {
             e.printStackTrace();
             return true;
+        }
+    }
+
+    public List<PhieuKhamBenh> getAll() {
+        try ( Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("FROM PhieuKhamBenh", PhieuKhamBenh.class).list();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
+    }
+
+    public List<PhieuKhamBenh> findByBenhNhanId(int benhNhanId) {
+        try ( Session session = HibernateUtil.getSessionFactory().openSession()) {
+            String hql = "SELECT FROM PhieuKhamBenh p WHERE p.benhNhan.id = :ma";
+            Query<PhieuKhamBenh> query = session.createQuery(hql, PhieuKhamBenh.class);
+            query.setParameter("ma", benhNhanId);
+            return query.list();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Collections.emptyList();
         }
     }
 }
